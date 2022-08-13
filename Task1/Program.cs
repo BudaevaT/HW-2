0df45c83b8a22385fa8278e37d6481bc264c4d14
Bildushkina Tuyana <Budaeva.tuyana.garmaevna@gmail.com>
2022-08-13 07:05:22 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трёхзначное число");
+int Number = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(Number);
+Console.WriteLine("Вторая цифра = " + stringNumber[1]);
