c0f7da473b6ad1b5603417e17a2fef2ce8036c85
Bildushkina Tuyana <Budaeva.tuyana.garmaevna@gmail.com>
2022-08-13 08:26:48 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingDayOfWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Да, выходной.");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("Неправильно задана цифра.");
+    }
+    else Console.WriteLine("Нет, не выходной.");
+}
+
+CheckingDayOfWeek(dayNumber);
